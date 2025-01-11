04450f8d0f760fa6b5ada9d29e096d615f9b6275
eddie g. <eduardgavrila47@gmail.com>
2025-01-11 23:51:43 +0100

    test

--- install.sh
+++ install.sh
@@ -93,10 +93,10 @@ fi
 # Format partitions and set labels
 clear
 echo "${bold}${yellow}Formatting partitions...${normal}"
-mkfs.fat -F32 -n -f "efi" "$efi_part"
+mkfs.fat -F32 -n "efi" "$efi_part"
 mkfs.btrfs -L -f "$hostname" "$btrfs_part"
 if [[ "$created_swap" == "yes" ]]; then
-    mkswap -L -f "swap" "$swap_part"
+    mkswap -L "swap" "$swap_part"
     swapon "$swap_part"
 fi
 
